1a1bfc51f17b210dac2e8df4a97d59a35a3afacf
Shahin shokoui <shahin@shahin.de>
2024-12-10 13:40:02 +0100

    Initial Commit

---
+++ Counter4/Counter4App.swift
@@ -0,0 +1,17 @@
+//
+//  Counter4App.swift
+//  Counter4
+//
+//  Created by Shahin on 10.12.24.
+//
+
+import SwiftUI
+
+@main
+struct Counter4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Counter4Tests/Counter4Tests.swift
@@ -0,0 +1,17 @@
+//
+//  Counter4Tests.swift
+//  Counter4Tests
+//
+//  Created by Shahin on 10.12.24.
+//
+
+import Testing
+@testable import Counter4
+
+struct Counter4Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
